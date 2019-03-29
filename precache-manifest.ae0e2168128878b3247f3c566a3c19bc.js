4ef38de062c446505bf7830cf6f9ad1416f94b28
foreseaz <foreseazhang@gmail.com>
2019-03-29 15:57:22 +0800

    ship: deployment

---
+++ precache-manifest.ae0e2168128878b3247f3c566a3c19bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a1e4e18baff0ffc8cda",
+    "url": "/js/about.d12686c2.js"
+  },
+  {
+    "revision": "58e545b910ea8171b044",
+    "url": "/css/app.2477bc96.css"
+  },
+  {
+    "revision": "58e545b910ea8171b044",
+    "url": "/js/app.eb30ac12.js"
+  },
+  {
+    "revision": "286a1f9439b7659aa988",
+    "url": "/css/chunk-vendors.d4285767.css"
+  },
+  {
+    "revision": "286a1f9439b7659aa988",
+    "url": "/js/chunk-vendors.93ce1028.js"
+  },
+  {
+    "revision": "99f8d289071da8e8f27688f4590cfbc9",
+    "url": "/img/bg.99f8d289.png"
+  },
+  {
+    "revision": "61c7d0a33461f27d611434abfed35960",
+    "url": "/img/logoicon.61c7d0a3.svg"
+  },
+  {
+    "revision": "d66e0aaa626bbd182da9eaa74d65f21c",
+    "url": "/img/logo.d66e0aaa.svg"
+  },
+  {
+    "revision": "dcb7abd5785f4708989bfac431ca9d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2f2df17ef54f9f61cdc6b9fb30aa43d8",
+    "url": "/logo.png"
+  }
+];
